9a021fc63c602fa4d899f3b0a681dccf960a93d2
marc-adaptive <marc.pinto@weareadaptive.com>
2024-07-21 15:19:25 -0400

    advance2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     implementation(libs.commons.text)
     implementation(libs.minio)
     implementation(libs.jackson)
-    implementation(libs.spring)
 
     constraints {
         // Force a newer version of commons-compress in transitive resolution
